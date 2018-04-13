c5224617766ce5b22b2e4339f7fb3ae49f149595
Stephen <stephen@skillfulgorilla.com>
2018-04-13 23:19:43 +0100

    Added deal method. Did an exercise on pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -21,12 +21,15 @@ defmodule Cards do
     end
   end
 
-
   def shuffle(deck) do
     Enum.shuffle(deck)     
   end
 
   def contains?(deck, card) do
     Enum.member?(deck, card) 
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size) 
   end
 end
